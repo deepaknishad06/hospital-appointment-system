ddb9d1316fce36150351153fd368340b0434e66f
Deepak Nishad <dcreation2706@gmail.com>
2026-07-21 12:48:52 +0530

    \\

--- server/server.js
+++ server/server.js
@@ -15,7 +15,26 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      "https://hospital-appointment-system-sooty.vercel.app",
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+    ];
+
+    if (!origin || allowedOrigins.includes(origin) || /^https:\/\/.*\.vercel\.app$/i.test(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 connectDB().catch((error) => {
